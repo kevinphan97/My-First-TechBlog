6ff4b468bad6fd7b2eaa5de4d7da3f928cdbba5c
Kevin Phan <kevin.b.phan97@gmail.com>
2021-09-18 02:00:59 -0700

    Server and Util added

---
+++ server.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const hbs = exphbs.create({helpers});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+const sess = {
+    secret: 'Secret Stuff',
+    cookie: {expires: 300000},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+});
